055178f87ac9e49df1e3f7f8bab84ec49413da00
Jason Vance <jbvance@gmail.com>
2020-02-08 14:40:25 -0600

    chagned app.js text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi, Silly
+          Hi, there! Let's get started
         </p>
         <a
           className="App-link"
